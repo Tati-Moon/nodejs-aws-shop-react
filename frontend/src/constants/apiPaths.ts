cd1117baca83d7df202616d3571fe61cc78bffbf
Tati <tati.moon89@gmail.com>
2024-08-04 23:16:43 +0200

    Update order and cart urls

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://dapdmi8g8h.execute-api.us-east-1.amazonaws.com/prod",
-  order: 'https://7gfwc5rdqhja7nnbl5lhsnqcki0chtgh.lambda-url.us-east-1.on.aws/api',
+  order: 'http://tatimoon-cart-api-dev.us-east-1.elasticbeanstalk.com/api',
   import: "https://j8xxg9c80l.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://dapdmi8g8h.execute-api.us-east-1.amazonaws.com/prod",
-  cart:"https://7gfwc5rdqhja7nnbl5lhsnqcki0chtgh.lambda-url.us-east-1.on.aws/api",
+  cart:"http://tatimoon-cart-api-dev.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
